9ba354418bc748400c977755474183714ebb6f49
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-26 21:05:43 +0100

    Each instrument has associated sound and getSound method

--- src/main/java/instruments/Sounds.java
+++ src/main/java/instruments/Sounds.java
@@ -1,4 +1,19 @@
 package instruments;
 
 public enum Sounds {
+    STRING("Strum"),
+    WOODWIND("tootle"),
+    BRASS("toooot"),
+    KEYBOARD("plink");
+
+    public final String sound;
+
+    Sounds(String sound){
+        this.sound = sound;
+    }
+
+    public String getSound() {
+        return this.sound;
+    }
+
 }
