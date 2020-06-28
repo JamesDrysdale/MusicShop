21c55c03d46926450204072fee6740307e125751
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-28 08:13:42 +0100

    Test that trumpet can play sound

--- src/test/java/instruments/TrumpetTest.java
+++ src/test/java/instruments/TrumpetTest.java
@@ -1,4 +1,23 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TrumpetTest {
+    Trumpet trumpet;
+
+    @Before
+    public void before(){
+        trumpet = new Trumpet();
+    }
+
+    @Test
+    public void trumpetCanPlaySound(){
+        trumpet.play(){
+            assertEquals("D", trumpet.play("D"));
+        }
+    }
+
 }
