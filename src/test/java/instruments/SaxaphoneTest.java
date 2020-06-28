e3730c4c4f8a8d16ff39f313c5cb9ed10484da22
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-28 08:08:40 +0100

    Add test: Saxaphone can play sound

--- src/test/java/instruments/SaxaphoneTest.java
+++ src/test/java/instruments/SaxaphoneTest.java
@@ -1,4 +1,23 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SaxaphoneTest {
+    Saxaphone saxaphone;
+
+    @Before
+    public void before(){
+        saxaphone = new Saxaphone();
+    }
+
+    @Test
+    public void saxaphoneCanPlaySound(){
+        saxaphone.play("A C B C A");
+        assertEquals("A C B C A", saxaphone.play("A C B C A"));
+    }
+
+
 }
