dd4a22d6d91f667429881d8085b4e91534d637e5
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-28 07:43:59 +0100

    Guitars can play sounds

--- src/test/java/instruments/GuitarTest.java
+++ src/test/java/instruments/GuitarTest.java
@@ -1,4 +1,24 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+    Guitar bassGuitar;
+
+    @Before
+    public void before(){
+        bassGuitar = new Guitar();
+    }
+
+    @Test
+    public void instrumentCanPlaySound(){
+        bassGuitar.play("C#");
+        assertEquals("C#", bassGuitar.play("C#"));
+    }
+
+
+
 }
